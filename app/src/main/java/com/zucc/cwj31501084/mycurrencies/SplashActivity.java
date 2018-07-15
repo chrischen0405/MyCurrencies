7de744a9d25c659d1a29e04d0535faaffca9fc45
chenwenjie <chrischen@gmail.com>
2018-07-15 14:01:39 +0800

    JSONParser类创建，SplashActivity创建

---
+++ app/src/main/java/com/zucc/cwj31501084/mycurrencies/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.zucc.cwj31501084.mycurrencies;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+    }
+}
