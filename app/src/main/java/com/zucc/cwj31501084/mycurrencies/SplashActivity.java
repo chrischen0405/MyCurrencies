080f40aeab94684a6f5b8f4b22f9984f9a6beee1
chenwenjie <chrischen0405@gmail.com>
2018-07-16 14:32:34 +0800

    SplashActivity加载MainActivity

--- app/src/main/java/com/zucc/cwj31501084/mycurrencies/SplashActivity.java
+++ app/src/main/java/com/zucc/cwj31501084/mycurrencies/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.zucc.cwj31501084.mycurrencies;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Window;
@@ -16,6 +17,7 @@ public class SplashActivity extends Activity {
     //url to currency codes used in this application
     public static final String URL_CODES =
             "http://openexchangerates.org/api/currencies.json";
+    public static final String KEY_ARRAYLIST = key_arraylist;
     //ArrayList of currencies that will be fetched and passed into MainActivity
     private ArrayList<String> mCurrencies;
 
@@ -41,6 +43,9 @@ public class SplashActivity extends Activity {
                     key = (String) iterator.next();
                     mCurrencies.add(key + " | " + jsonObject.getString(key));
                 }
+                Intent mainIntent =new Intent(SplashActivity.this,MainActivity.class);
+                mainIntent.putExtra( KEY_ARRAYLIST,mCurrencies);
+                startActivity(mainIntent);
                 finish();
             } catch (JSONException e) {
                 Toast. makeText(
