2e9251f7dc22b27c467271319abb61b52be4cda9
chenwenjie <chrischen0405@gmail.com>
2018-07-21 22:28:27 +0800

    汇率一个小时自动更新一次

--- app/src/main/java/com/zucc/cwj31501084/mycurrencies/MyService.java
+++ app/src/main/java/com/zucc/cwj31501084/mycurrencies/MyService.java
@@ -4,13 +4,27 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.os.SystemClock;
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Properties;
+
+import static com.zucc.cwj31501084.mycurrencies.MainActivity.RATES;
+import static com.zucc.cwj31501084.mycurrencies.MainActivity.TIMESTAMP;
+import static com.zucc.cwj31501084.mycurrencies.MainActivity.URL_BASE;
 
 public class MyService extends Service {
+
     public MyService() {
     }
 
@@ -32,7 +46,8 @@ public class MyService extends Service {
         }).start();
         //获取AlarmManager 实例
         AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        int anHour = 10 * 1000;
+        new UpdateRateTask().execute(URL_BASE + getKey());
+        int anHour = 60 * 60 * 1000;
         //触发时间
         long triggerAtTime = SystemClock.elapsedRealtime() + anHour;
         Intent i = new Intent(this, AlarmReceiver.class);
@@ -41,6 +56,62 @@ public class MyService extends Service {
         manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtTime, pendingIntent);
 
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    private String getKey() {
+        AssetManager assetManager = this.getResources().getAssets();
+        Properties properties = new Properties();
+        try {
+            InputStream inputStream = assetManager.open("keys.properties");
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties.getProperty("open_key");
+    }
+
+    private class UpdateRateTask extends AsyncTask<String, Void, JSONObject> {
+        private long ratetime;
+        private String allrate;
+
+
+        @Override
+        protected void onPostExecute(JSONObject jsonObject) {
+            try {
+
+                if (jsonObject == null) {
+                    throw new JSONException("no data available.");
+                }
+                JSONObject jsonRates = jsonObject.getJSONObject(RATES);
+                allrate = jsonRates.toString();
+
+                Log.i("jsonrates", jsonRates.toString());
+                ratetime = jsonObject.getLong(TIMESTAMP);
+                Log.i("jsontime", ratetime + "");
+            } catch (JSONException e) {
+                Log.i("exception: ", e.getMessage());
+                e.printStackTrace();
+            }
+
+            BeanAllRate data = new BeanAllRate();
+            int flag = 0;
+            data.setAllrate(allrate);
+            data.setRatetime(ratetime);
+            MyDatabaseManager dbManager = new MyDatabaseManager(getBaseContext());
+            ArrayList<BeanAllRate> all = dbManager.queryAllRate();
+            for (BeanAllRate d : all) {
+                if (d.getRatetime() == ratetime)
+                    flag = 1;
+            }
+            if (flag == 0)
+                dbManager.addData(data);
+
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... strings) {
+            return new JSONParser().getJSONFromUrl(strings[0]);
+        }
     }
 
 }
